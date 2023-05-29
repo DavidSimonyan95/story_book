11642db2e7ef461a158d8af551168863bfe42b7b
DavidSimonyan95 <davo.simonyan1195@gmail.com>
2023-05-29 14:09:24 +0400

    install storyBook

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
 import "./App.css";
-import { Dialog } from "./components/facyBorder";
-import { Chat, Contacts, SplitPane } from "./components/splitPane";
+
 function App() {
-  return (
-    <div className="App">
-      <Dialog title="Welcome" message="Thank you for visiting" />
-      <SplitPane left={<Contacts />} right={<Chat />} />
-    </div>
-  );
+  return <div className="App">App</div>;
 }
 
 export default App;
